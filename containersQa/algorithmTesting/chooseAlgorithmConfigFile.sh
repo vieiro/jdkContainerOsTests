17497fd2f8b872f11e3b5d2181fa51a171609d11
Jiří Andrlík <jandrlik@redhat.com>
2025-09-04 13:19:30 +0200

    rework of the previous approach and simplification of the choosing script

--- containersQa/algorithmTesting/chooseAlgorithmConfigFile.sh
+++ containersQa/algorithmTesting/chooseAlgorithmConfigFile.sh
@@ -5,49 +5,29 @@ function chooseAlgorithmConfigFile() {
     echo "Needs three arguments. First one should be the version of RHEL and second one should be 'true' or 'fips', if FIPS mode is on. Last one should be the java version."
     exit 1
   fi
-
-  if [ "$1" == "el.7" -o "$1" == "el.7z" ] ; then
-    if [ "$2" == "true" -o "$2" == "fips" ] ; then
-      echo "el7ConfigFips.txt"
-    else
-      echo "el7ConfigLegacy.txt"
+  
+  osPart="fedora"
+  if [ "20" -ge "$1" ] ; then
+    osPart="el$1"
+    if [ "$1" -gt 10 ] ; then
+      echo "Unknown/unsupported RHEL version was specified: $1"
+      exit 1
     fi
-  elif [ "$1" == "el.8" -o "$1" == "el.8z" -o "$1" == "el.8z2" -o  "$1" == "el.8z4" -o  "$1" == "el.8z6" -o  "$1" == "el.8z8" ] ; then
-    if [ "$2" == "true" -o "$2" == "fips" ] ; then
-      if [ "$3" == "8" -o "$3" == "11" ] ; then
-	echo "el8ConfigFips11Older.txt"
-      else
-        echo "el8ConfigFips.txt"
-      fi
-    else
-      echo "el8ConfigLegacy.txt"
-    fi
-  elif [ "$1" == "el.9" -o "$1" == "el.9z" -o "$1" == "el.9z2" -o "$1" == "el.9z0" ] ; then
-    if [ "$2" == "true" -o "$2" == "fips" ] ; then
-      if [ "$3" == "8" -o "$3" == "11" ] ; then
-        echo "el9ConfigFips11Older.txt"
-      else
-        echo "el9ConfigFips.txt"
-      fi
-    else
-      echo "el9ConfigLegacy.txt"
-    fi
-  elif [ "$1" == "el.10" ] ; then
-    if [ "$2" == "true" -o "$2" == "fips" ] ; then
-      echo "el10ConfigFips.txt"
-    else
-      echo "el10ConfigLegacy.txt"
-    fi
-  elif [ "$1" == "f.41" ] ; then
-    if [ "$2" == "true" -o "$2" == "fips" ] ; then
-      echo "fedoraConfigFips.txt"
-    else
-      echo "fedoraConfigLegacy.txt"
-    fi
-  else
-    echo "Unknown/unsupported RHEL version was specified: $1"
-    exit 1
   fi
+
+  fipsPart="Legacy"
+  if [ "$2" == "true" -o "$2" == "fips" ] ; then
+    fipsPart="Fips"
+  fi
+
+  # so far only fips had differences with older jdks
+  suffix=""
+  if [ "$3" == "8" -o "$3" == "11" ] ; then
+    suffix="OldJdks"
+  fi
+
+  echo "${osPart}Config${fipsPart}${suffix}.txt"
+
 }
 
 chooseAlgorithmConfigFile "${1}" "${2}" "${3}"
